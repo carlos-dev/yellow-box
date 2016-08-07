caa2901253d593818e6f44426800391327e2636f
Carlos Sánchez <sanccarmo@gmail.com>
2016-08-07 11:34:18 +0200

    randomize files

--- randomPlayer.js
+++ randomPlayer.js
@@ -1,9 +1,10 @@
 var fs = require('fs')
+var shuffle = require('lodash/shuffle')
 require('dotenv').config()
 
 fs.readdir(process.env.MUSIC_FOLDER, (err, files) => {
   if (err) {
     throw Error(err.message)
   }
-  console.log(files)
+  console.log(shuffle(files))
 })
