03f7671ac31a44b0cd06c898a940eb145df9be46
Carlos Sánchez <sanccarmo@gmail.com>
2016-08-07 12:30:50 +0200

    play random playlist

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
+require('dotenv').config()
 var Player = require('player')
+var colors = require('colors')
+var fs = require('fs')
+var shuffle = require('lodash/shuffle')
 
-const player = new Player('./music/test.mp3')
+const addPath = (fileName) => {
+  return `${process.env.MUSIC_FOLDER}/${fileName}`
+}
 
-player.play(({player}) => {
-  console.log('play end!')
+const isMP3 = (fileName) => {
+  return /\.mp3$/.test(fileName)
+}
+
+const getRandomizedPlaylist = () => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(process.env.MUSIC_FOLDER, (err, files) => {
+      if (err) {
+        reject(err.message)
+      }
+      resolve(shuffle(files.filter(isMP3).map(addPath)))
+    })
+  })
+}
+
+getRandomizedPlaylist().then((files) => {
+  var player = new Player(files)
+  console.log(colors.yellow('Playlist: ' + files))
+  player.play()
+  player.on('playing', ({src, _name}) => {
+    console.log(colors.red('Playing ' + _name))
+  })
+  player.on('error', () => {
+    // playlist end
+    player.play()
+  })
 })
-
-setTimeout(() => {
-  player.stop()
-}, 3000)
